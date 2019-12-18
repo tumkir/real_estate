ce21b64e801c919ad373eab20823ea5eaddfeb33
tumkir <tumkir@gmail.com>
2019-12-18 19:14:00 +0300

    new_building доступно для редактирования на общей странице квартир

--- property/admin.py
+++ property/admin.py
@@ -5,5 +5,7 @@ from .models import Flat
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
-    readonly_fields = ["created_at", ]
-    search_fields = ['town', 'address', 'owner']
+    readonly_fields = ("created_at", )
+    search_fields = ("town", "address", "owner")
+    list_display = ("address", "price", "new_building", "construction_year", "town")
+    list_editable = ("new_building",)
