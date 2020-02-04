f6eb39b0c00cfd6e79f88175d25d170cb2c4f95f
tumkir <tumkir@gmail.com>
2020-02-04 13:10:57 +0300

    Перенос связи собственник-квартира из модели Flat в модель Owner

--- property/admin.py
+++ property/admin.py
@@ -6,9 +6,9 @@ from .models import Flat, Complaint, Owner
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ("created_at", )
-    search_fields = ("town", "address", "owner")
-    list_display = ("address", "owners_phonenumber", "owner_phone_pure", "price", "new_building", "construction_year", "town")
-    list_editable = ("new_building",)
+    search_fields = ("town", "address")
+    list_display = ("address", "price", "new_building", "construction_year", "town")
+    list_editable = ("new_building", )
     list_filter = ("new_building", "rooms_number", "has_balcony")
     raw_id_fields = ('likes', )
 
--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
-    owners_phonenumber = models.CharField("Номер владельца", max_length=20)
-    owner_phone_pure = PhoneNumberField("Нормализованный номер владельца", blank=True)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
 
     description = models.TextField("Текст объявления", blank=True)
@@ -39,7 +36,7 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
+    owner = models.CharField("ФИО владельца", max_length=200, db_index=True)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     owner_phone_pure = PhoneNumberField("Нормализованный номер владельца", blank=True)
-    owners_flats = models.ManyToManyField(Flat, related_name="owners", verbose_name="Квартиры в собственности")
+    owners_flats = models.ManyToManyField(Flat, related_name="owners", verbose_name="Квартиры в собственности", db_index=True)
